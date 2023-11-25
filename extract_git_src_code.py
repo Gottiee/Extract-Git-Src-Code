e4607bd21f6af8469975dcb918b0d9329ef7b679
gottie <gottiedev@gmail.com>
2023-11-25 18:33:54 +0100

    create all dir recursively

--- extract_git_src_code.py
+++ extract_git_src_code.py
@@ -2,8 +2,7 @@
 
 import argparse, os, requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, urljoin
 from pathlib import Path
 
 def create_directory(directory_path):
@@ -44,18 +43,26 @@ def add_git_path(url: str):
     if not parsed_url.path.endswith(".git"):
         new_path = parsed_url.path + "/.git"
         modified_url = urlunparse(parsed_url._replace(path=new_path))
-        return modified_url
-    return url
+        return modified_url + "/"
+    return url + "/"
 
-def dowload_git_folder(url:str, depth=1):
+def dowload_git_folder(url:str, path: Path, depth=1):
     try:
+        print(f"URL {url}")
+        print(f"Path {str(path)}")
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a', href=True) 
         for link in links:
-            if link['href'].endswith('/') : 
-                create_directory
+            dir = link['href']
+            if dir.endswith('/') and dir != "../": 
+                new_url = url + dir
+                dir = dir[:-1]
+                new_path = path / dir
+                # create_directory(str(new_path))
+                dowload_git_folder(new_url, new_path)
+
 
     except requests.exceptions.RequestException as e:
         print(f"Error dowloading {url}: {e}")
@@ -63,11 +70,12 @@ def dowload_git_folder(url:str, depth=1):
 def main():
     args = argument()
     url = add_git_path(args.url)
-    create_directory(args.output)
+    # create_directory(args.output)
     base = Path(args.output)
-    create_directory(str(base / ".git"))
+    path = base / ".git"
+    # create_directory(str(path))
     if not args.blind:
-        dowload_git_folder(url)
+        dowload_git_folder(url, path)
 
 if __name__ == "__main__":
     main()
