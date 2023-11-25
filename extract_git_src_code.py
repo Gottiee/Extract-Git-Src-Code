0f0843d26883c925065167de21ac2b37e65662bd
gottie <gottiedev@gmail.com>
2023-11-25 19:02:44 +0100

    it download everythink recursively

--- extract_git_src_code.py
+++ extract_git_src_code.py
@@ -18,7 +18,7 @@ def create_directory(directory_path):
 
 def create_file(file_path, content=None):
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, 'wb') as file:
             if content is not None:
                 file.write(content)
         if os.path.exists(file_path) and os.path.isfile(file_path):
@@ -46,23 +46,33 @@ def add_git_path(url: str):
         return modified_url + "/"
     return url + "/"
 
+def dowload_file(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print(f"Error dowloading {url}: {e}")
+
 def dowload_git_folder(url:str, path: Path, depth=1):
     try:
-        print(f"URL {url}")
-        print(f"Path {str(path)}")
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a', href=True) 
         for link in links:
             dir = link['href']
-            if dir.endswith('/') and dir != "../": 
+            if dir == "../":
+                continue
+            if dir.endswith('/'): 
                 new_url = url + dir
                 dir = dir[:-1]
                 new_path = path / dir
-                # create_directory(str(new_path))
+                create_directory(str(new_path))
                 dowload_git_folder(new_url, new_path)
-
+            else:
+                file_path = path / dir
+                print(f"Dowloading: {str(file_path)}")
+                create_file(str(file_path), dowload_file(url + dir))
 
     except requests.exceptions.RequestException as e:
         print(f"Error dowloading {url}: {e}")
@@ -70,10 +80,10 @@ def dowload_git_folder(url:str, path: Path, depth=1):
 def main():
     args = argument()
     url = add_git_path(args.url)
-    # create_directory(args.output)
+    create_directory(args.output)
     base = Path(args.output)
     path = base / ".git"
-    # create_directory(str(path))
+    create_directory(str(path))
     if not args.blind:
         dowload_git_folder(url, path)
 
