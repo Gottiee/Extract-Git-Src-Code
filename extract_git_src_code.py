8303dd002fc87604a1acddf56b6a0ae0f8836546
gottie <gottiedev@gmail.com>
2023-11-26 13:39:29 +0100

    extract head from .git

--- extract_git_src_code.py
+++ extract_git_src_code.py
@@ -31,10 +31,12 @@ def create_file(file_path, content=None):
 
 def argument():
     parser  = argparse.ArgumentParser(description="Extract git src code, extract /.git from a given URL and try to recompose the src code")
-    parser.add_argument("-u", "--url", help="path/to/git/folder/.git", required=True)
-    parser.add_argument('-b', '--blind', action='store_true', help="No directory listing")
+    parser.add_argument("-u", "--url", help="https://url.net/path/to/git/folder/.git", required=True)
     parser.add_argument('-o', '--output', help='Name of extract .git folder (default=\'git\')', default='git')
+    parser.add_argument('-b', '--blind', action='store_true', help="No directory listing")
     parser.add_argument('-v', '--verbose', help='Print additional informations', action='store_true', default=False)
+    parser.add_argument('--onlyDownload',action='store_true', help="Do not extract source code, only '.git' directory", default=False)
+    parser.add_argument('--onlyExtract',action='store_true', help="Do not download '.git' directory and assume it already exist to perform the soure code extraction", default=False)
     return parser.parse_args()
 
 def add_git_path(url: str):
@@ -75,19 +77,51 @@ def dowload_git_folder(url:str, path: Path, verbose):
                 if verbose:
                     print(f"Dowloading: {str(file_path)}")
                 create_file(str(file_path), dowload_file(url + dir))
-
     except requests.exceptions.RequestException as e:
         print(f"Error dowloading {url}: {e}")
 
+def open_read(path: str):
+    try:
+        with open(str(path), 'r') as file:
+            return file.read()
+    except Exception as e:
+        print(f"File error at {str(path)} : {e}")
+        return None
+
+def get_src_code(path: Path):
+    head_path = path / "HEAD"
+    content = open_read(str(head_path))
+    if content == None:
+        print(f"End of the program")
+        return
+    if content.endswith("\n"):
+        content = content[:-1]
+    if content.startswith("ref: "):
+        head = content.split("ref: ", 1)[1]
+        path = path / head
+        git_head = open_read(str(path))
+        if git_head == None:
+            print(f"End of the program")
+            return
+        if git_head.endswith("\n"):
+            git_head = git_head[:-1]
+    else:
+        git_head = content
+    print(git_head)
+
+
 def main():
     args = argument()
-    url = add_git_path(args.url)
-    create_directory(args.output)
     base = Path(args.output)
     path = base / ".git"
-    create_directory(str(path))
-    if not args.blind:
-        dowload_git_folder(url, path, args.verbose)
+    if not args.onlyExtract:
+        url = add_git_path(args.url)
+        create_directory(args.output)
+        create_directory(str(path))
+        if not args.blind:
+            dowload_git_folder(url, path, args.verbose)
+    if not args.onlyDownload:
+        get_src_code(path)
 
 if __name__ == "__main__":
     main()
